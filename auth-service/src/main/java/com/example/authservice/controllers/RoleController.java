d27d7ddf9a78c3f593d4d58dd23650b0c86a52d2
JimySheepman <y.alikoyuncu@gmail.com>
2022-07-16 21:28:43 +0300

    refactoring auth service controllers

--- auth-service/src/main/java/com/example/authservice/controllers/RoleController.java
+++ auth-service/src/main/java/com/example/authservice/controllers/RoleController.java
@@ -4,6 +4,7 @@ import com.example.authservice.models.Role;
 import com.example.authservice.services.UserService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class RoleController {
     private final UserService userService;
 
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     public Role save(@RequestBody Role role){
         return userService.save(role);
     }
--- auth-service/src/main/java/com/example/authservice/controllers/UserController.java
+++ auth-service/src/main/java/com/example/authservice/controllers/UserController.java
@@ -4,30 +4,36 @@ import com.example.authservice.dto.AddRoleRequestDTO;
 import com.example.authservice.models.User;
 import com.example.authservice.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping
+    @PostMapping("/register")
+    public ResponseEntity<?> save(@RequestBody User user){
+        userService.save(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/api/v1/users")
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     public List<User> list(){
         return userService.list();
     }
 
-    @PostMapping
-    public User save(@RequestBody User user){
-        return userService.save(user);
-    }
 
-    @PatchMapping
+    @PatchMapping("/api/v1/users")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     public void addRoles(@RequestBody AddRoleRequestDTO request){
     userService.addRoleTo(request.getUsername(),request.getRole());
     }
